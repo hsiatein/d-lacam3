7b0c82589d520cbec95cfdf7014fa0f6870df8d4
XiaTian <3023625451@qq.com>
2025-04-23 10:23:28 +0800

    1

--- run.sh
+++ run.sh
@@ -7,9 +7,11 @@
 #map=tree2&&N=17
 #map=tunnel&&N=4
 #map=lak303d&&N=1000
-#map=random-32-32-10&&N=485
+#map=random-32-32-10-2&&N=485
+map=random-32-32-10&&N=460
 #map=maze-128-128-1&&N=350
-map=room-64-64-8&&N=1000
+#map=room-64-64-8&&N=1000
+#map=w_woundedcoast&&N=1000
 
 build/main -i assets/$map.scen -m assets/$map.map -N $N -v 3 --no-star
 mapf-visualizer assets/$map.map ./build/result.txt

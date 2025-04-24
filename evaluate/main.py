69cd1bbe00eab7ec737a26297349ab645d66b256
XiaTian <3023625451@qq.com>
2025-04-24 12:12:05 +0800

    1

--- evaluate/main.py
+++ evaluate/main.py
@@ -27,13 +27,64 @@ mapDict={0:["corners",4],
 
 Candidates=[8,10,11,12,13,14,15,16,17,18,19,20,21]
 #Candidates=[12]
-TieBreakers=['0','1','2','3','4']
+TieBreakers=['0','1','2','3','4','5']
 
-selected=16
 
-for selected in Candidates:
+# for selected in Candidates:
+#     print(mapDict[selected][0])
+#     print(mapDict[selected][1])
+#     costs=[]
+#     lbs=[]
+#     ubs=[]
+#     for tiebreaker in TieBreakers:
+#         # 构建命令
+#         compute = [
+#             'build/main',
+#             '-i', f'assets/{mapDict[selected][0]}.scen',
+#             '-m', f'assets/{mapDict[selected][0]}.map',
+#             '-N', str(mapDict[selected][1]),
+#             '-v', '3',
+#             '-t', '5',
+#             '--no-star',
+#             '--tie-breaker',tiebreaker,
+#         ]
+#         try:
+#             # 运行命令
+#             result_compute = subprocess.run(compute, capture_output=True, text=True, check=True)
+#             # 打印输出
+#             print("标准输出:")
+#             print(result_compute.stdout)
+#             print("标准错误:")
+#             print(result_compute.stderr)
+#             cost,lb,ub=utils.extract_total_cost(result_compute.stdout)
+#             costs.append(cost)
+#             lbs.append(lb)
+#             ubs.append(ub)
+#         except subprocess.CalledProcessError as e:
+#             print(f"命令执行失败: {e}")
+#             print(f"返回码: {e.returncode}")
+#             print(f"标准错误: {e.stderr}")
+#     x = list(range(len(ubs)))
+#     # 绘制柱状图
+#     plt.bar(x, ubs)
+#     # 添加标题和标签
+#     title=f'{mapDict[selected][0]} , {mapDict[selected][1]}'
+#     plt.title(title)
+#     plt.xlabel('tie breaker')
+#     plt.ylabel('ub')
+#     plt.ylim(bottom=1)
+#     for i in range(len(ubs)):
+#         plt.text(i, ubs[i]+0.01, str(round(ubs[i],4)), ha='center')
+#     plt.savefig(f'evaluate/fig/{title}.png')
+#     plt.clf()
+#     # 显示图形
+#     #plt.show()
+    
+selected=8
+nums=[50*i for i in range(1,31)]    
+for num in nums:
     print(mapDict[selected][0])
-    print(mapDict[selected][1])
+    print(num)
     costs=[]
     lbs=[]
     ubs=[]
@@ -43,7 +94,7 @@ for selected in Candidates:
             'build/main',
             '-i', f'assets/{mapDict[selected][0]}.scen',
             '-m', f'assets/{mapDict[selected][0]}.map',
-            '-N', str(mapDict[selected][1]),
+            '-N', str(num),
             '-v', '3',
             '-t', '5',
             '--no-star',
@@ -65,19 +116,21 @@ for selected in Candidates:
             print(f"命令执行失败: {e}")
             print(f"返回码: {e.returncode}")
             print(f"标准错误: {e.stderr}")
+            break
+    if len(ubs)==0:
+        break
     x = list(range(len(ubs)))
     # 绘制柱状图
     plt.bar(x, ubs)
     # 添加标题和标签
-    title=f'{mapDict[selected][0]} , {mapDict[selected][1]}'
+    title=f'{mapDict[selected][0]} , {num}'
     plt.title(title)
     plt.xlabel('tie breaker')
     plt.ylabel('ub')
     plt.ylim(bottom=1)
     for i in range(len(ubs)):
         plt.text(i, ubs[i]+0.01, str(round(ubs[i],4)), ha='center')
-    plt.savefig(f'evaluate/fig/{title}.png')
+    plt.savefig(f'evaluate/fig2/{title}.png')
     plt.clf()
     # 显示图形
     #plt.show()
-    

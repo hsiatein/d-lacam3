2a22676bc1b8553413c10cb94917335b452d032e
XiaTian <3023625451@qq.com>
2025-04-27 11:16:42 +0800

    1

--- run.sh
+++ run.sh
@@ -9,9 +9,10 @@
 #map=lak303d&&N=1000
 #map=random-32-32-10-2&&N=485
 #map=random-32-32-10&&N=460
-#map=maze-128-128-1&&N=350
-map=room-64-64-8&&N=1000
+map=maze-128-128-1&&N=400
+#map=room-64-64-8&&N=1000
 #map=w_woundedcoast&&N=1000
 
-build/main -i assets/$map.scen -m assets/$map.map -N $N -v 3 --tie-breaker 0 --no-star
+#build/main -i assets/maze-128-128-1.scen -m assets/maze-128-128-1.map -N 350 -v 3 --tie-breaker 0 -s 19 -t 5 --no-star
+build/main -i assets/$map.scen -m assets/$map.map -N $N -v 3 --tie-breaker 0 -s 18 -t 5 --no-star
 mapf-visualizer assets/$map.map ./build/result.txt
